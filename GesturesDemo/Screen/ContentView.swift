c3f53d804f9255adfd9eab92b86ce85bce6954de
Victor Ulloa <alejandroekas@gmail.com>
2023-10-02 01:18:41 -0400

    double tap zoom implemented

--- GesturesDemo/Screen/ContentView.swift
+++ GesturesDemo/Screen/ContentView.swift
@@ -8,16 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //MARK: - PROPERTY
+    
+    @State private var isAnimating: Bool = false
+    @State private var imageScale: CGFloat = 1
+    
+    //MARK: - FUNCTION
+    
+    //MARK: - CONTENT
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationView {
+            ZStack {
+                // MARK: - PAGE IMAGE
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0)
+                    .scaleEffect(imageScale)
+                // MARK: - 1. TAP GESTURE
+                    .onTapGesture(count: 2, perform: {
+                        if imageScale == 1 {
+                            withAnimation(.spring()) {
+                                imageScale = 5
+                            }
+                        } else {
+                            withAnimation(.spring()) {
+                                imageScale = 1
+                            }
+                        }
+                    })
+            } //: ZSTACK
+            .navigationTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear(perform: {
+                withAnimation(.linear(duration: 1)) {
+                    isAnimating = true
+                }
+            })
+        } // :NAVIGATION
+        .navigationViewStyle(.stack)
     }
 }
+
+//MARK: - PREVIEW
 
 #Preview {
     ContentView()
