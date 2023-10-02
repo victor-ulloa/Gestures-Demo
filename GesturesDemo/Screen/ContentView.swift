3a32701d13f491374fc21b65c02e57ac0cdeeda6
Victor Ulloa <alejandroekas@gmail.com>
2023-10-02 02:29:52 -0400

    info panel view implemented

--- GesturesDemo/Screen/ContentView.swift
+++ GesturesDemo/Screen/ContentView.swift
@@ -28,6 +28,7 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             ZStack {
+                Color.clear
                 // MARK: - PAGE IMAGE
                 Image("magazine-front-cover")
                     .resizable()
@@ -70,6 +71,14 @@ struct ContentView: View {
                     isAnimating = true
                 }
             })
+            // MARK: - INFO PANEL
+            .overlay(
+                InfoPanelView(scale: imageScale, offset: imageOffset)
+                    .padding(.horizontal)
+                    .padding(.top, 30)
+                , alignment: .top
+            )
+                
         } // :NAVIGATION
         .navigationViewStyle(.stack)
     }
