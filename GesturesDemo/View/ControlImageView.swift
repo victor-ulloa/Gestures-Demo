32099dd18007c56cf6460e4a07f587b79e723509
Victor Ulloa <alejandroekas@gmail.com>
2023-10-02 15:16:48 -0400

    zoom controls implemented

---
+++ GesturesDemo/View/ControlImageView.swift
@@ -0,0 +1,24 @@
+//
+//  ControlImageView.swift
+//  GesturesDemo
+//
+//  Created by Victor Ulloa on 2023-10-02.
+//
+
+import SwiftUI
+
+struct ControlImageView: View {
+    let icon: String
+    
+    var body: some View {
+        Image(systemName: icon)
+            .font(.system(size: 36))
+    }
+}
+
+#Preview {
+    ControlImageView(icon: "minus.magnifyingglass")
+        .preferredColorScheme(.dark)
+        .previewLayout(.sizeThatFits)
+        .padding()
+}
