730acd4a1c5a48ebea2e78327b9bb8fa77dbb6f4
Victor Ulloa <alejandroekas@gmail.com>
2023-10-02 15:24:07 -0400

    pinch gesture implemented

--- GesturesDemo/Screen/ContentView.swift
+++ GesturesDemo/Screen/ContentView.swift
@@ -49,19 +49,39 @@ struct ContentView: View {
                             resetImageState()
                         }
                     })
-                // MARK: - DRAG GESTURE
+                // MARK: - 2. DRAG GESTURE
                     .gesture(
                         DragGesture()
-                            .onChanged({ value in
+                            .onChanged { value in
                                 withAnimation(.linear(duration: 1)) {
                                     imageOffset = value.translation
                                 }
-                            })
-                            .onEnded({ _ in
+                            }
+                            .onEnded { _ in
                                 if imageScale <= 1 {
                                     resetImageState()
                                 }
-                            })
+                            }
+                    )
+                // MARK: - 3. MAGNIFICATION GESTURE
+                    .gesture (
+                        MagnificationGesture()
+                            .onChanged { value in
+                                withAnimation(.linear(duration: 1)) {
+                                    if imageScale >= 1 && imageScale <= 5 {
+                                        imageScale = value
+                                    } else if imageScale > 5 {
+                                        imageScale = 5
+                                    }
+                                }
+                            }
+                            .onEnded { _ in
+                                if imageScale > 5 {
+                                    imageScale = 5
+                                } else if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                            }
                     )
             } //: ZSTACK
             .navigationTitle("Pinch & Zoom")
