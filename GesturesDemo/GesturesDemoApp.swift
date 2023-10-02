1ef182e688e71929ec3dc7a2006f2b75534c7756
Victor Ulloa <alejandroekas@gmail.com>
2023-10-01 23:49:29 -0400

    Initial Commit

---
+++ GesturesDemo/GesturesDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GesturesDemoApp.swift
+//  GesturesDemo
+//
+//  Created by Victor Ulloa on 2023-10-01.
+//
+
+import SwiftUI
+
+@main
+struct GesturesDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
